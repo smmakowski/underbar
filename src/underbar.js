c117c947c99013e8f755d276c7324793b2162214
smmakowski <makowskowski.sm@gmail.com>
2017-01-21 18:45:03 -0800

    "Part 1 Done; completed .extend and .defaults in Part 2"

--- src/underbar.js
+++ src/underbar.js
@@ -202,12 +202,14 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+
   };
 
 
@@ -230,11 +232,31 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      var otherObj = arguments[i];
+
+      for (var key in otherObj) {
+        obj[key] = otherObj[key];
+      }
+    }
+
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      var otherObj = arguments[i];
+
+      for (var key in otherObj) {
+          if (obj[key] === undefined) {
+            obj[key] = otherObj[key];
+          }
+        }
+      }
+
+    return obj;
   };
 
 
